907ff89c33fe3152983e78981be5b69b8f032220
abdizamedmo <abdizamedmo@gmail.com>
2023-01-30 12:47:52 +0300

    Profile page is refactored but there is an error and not fixed

--- src/screens/profile/profile.tsx
+++ src/screens/profile/profile.tsx
@@ -1,8 +1,18 @@
-import { Text, View, Image, TouchableOpacity, Platform } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Platform,
+  SectionList,
+  Linking,
+} from "react-native";
 import React from "react";
 import { useTheme } from "@/theme/ThemeProvider";
 import { Ionicons } from "@expo/vector-icons";
 import * as ImagePicker from "expo-image-picker";
+import { fonts } from "@/constants/fonts";
+import { Button } from "@/components";
 
 const fakeUsers = [
   {
@@ -25,7 +35,7 @@ const fakeProfileUsers = [
   {
     userId: 1,
     website: "https://johndoe.com",
-    photoURL: "https://avatars.githubusercontent.com/u/98351506?v=4",
+    photoURL: "https://avatars.githubusercontent.com/u/96237528?v=4",
     bio: "I am John Doe",
     address: {
       street: "123 Main St",
@@ -176,12 +186,13 @@ export default function ProfileScreen({
         backgroundColor: colors.primary,
       }}
     >
-      <View
+      <TouchableOpacity
         style={{
           alignItems: "center",
-          //   marginHorizontal: 20,
-          backgroundColor: colors.primary,
-          padding: 25,
+          justifyContent: "center",
+          height: 200,
+          backgroundColor: !dark ? "#e5e5e5" : "#11121a",
+          //   colors.primary
           marginTop: 20,
           shadowColor: dark ? "#000" : "#d1d5db",
           shadowOffset: {
@@ -190,78 +201,262 @@ export default function ProfileScreen({
           },
           shadowOpacity: 0.41,
           shadowRadius: 92.11,
-
           elevation: 14,
         }}
       >
         <View
           style={{
-            width: 100,
-            height: 100,
-            borderRadius: 50,
-            backgroundColor: colors.text,
-            justifyContent: "center",
+            flexDirection: "row",
           }}
         >
-          <TouchableOpacity onPress={() => openFullScreenImage()}>
-            {userInfo.photoURL ? (
-              <Image
-                source={{ uri: userInfo.photoURL }}
-                style={{
-                  width: 120,
-                  height: 120,
-                  borderRadius: 99,
-                }}
-              />
-            ) : (
-              <Text
-                style={{
-                  textAlign: "center",
-                  fontSize: 30,
-                  color: colors.primary,
-                }}
-              >
-                {userInfo?.name?.charAt(0)}
-              </Text>
-            )}
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            onPress={() => pickImage()}
+          <Text
             style={{
-              position: "absolute",
-              bottom: -5,
-              right: -15,
-              backgroundColor: colors.primary,
-              borderRadius: 50,
+              color: colors.text,
+              fontFamily: fonts.primary.regular,
+              fontSize: 18,
+              opacity: 0.4,
             }}
           >
-            <Ionicons
-              name="camera-outline"
-              size={24}
-              color={dark ? "#e5e7eb" : "#1f2937"}
-            />
-          </TouchableOpacity>
+            Add
+          </Text>
+
+          <Ionicons
+            name="camera-outline"
+            size={24}
+            color={colors.text}
+            style={{
+              marginLeft: 10,
+              opacity: 0.4,
+            }}
+          />
+          <Text
+            style={{
+              color: colors.text,
+              fontFamily: fonts.primary.regular,
+              fontSize: 18,
+              marginLeft: 10,
+              opacity: 0.4,
+            }}
+          >
+            Cover photo
+          </Text>
         </View>
-        <Text
+      </TouchableOpacity>
+
+      <View
+        style={{
+          position: "absolute",
+          top: 150,
+          left: 40,
+
+          width: 100,
+          height: 100,
+          borderRadius: 50,
+          backgroundColor: colors.text,
+          justifyContent: "center",
+          alignItems: "flex-end",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => openFullScreenImage()}
           style={{
-            fontSize: 25,
-            color: colors.text,
-            marginTop: 5,
+            borderWidth: 3,
+            borderColor: colors.primary,
+            borderRadius: 90,
+            padding: 0.5,
           }}
         >
-          {userInfo?.name}
-        </Text>
-        <Text
+          {userInfo.photoURL ? (
+            <Image
+              source={{ uri: userInfo.photoURL }}
+              style={{
+                width: 120,
+                height: 120,
+                borderRadius: 100,
+              }}
+            />
+          ) : (
+            <Text
+              style={{
+                textAlign: "center",
+                fontSize: 30,
+                color: colors.primary,
+              }}
+            >
+              {userInfo?.name?.charAt(0)}
+            </Text>
+          )}
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => pickImage()}
           style={{
-            fontSize: 15,
-            color: colors.text,
-            marginTop: 0,
+            position: "absolute",
+            bottom: -5,
+            right: 5,
+            backgroundColor: colors.primary,
+            borderRadius: 50,
           }}
         >
-          {userInfo?.bio}
-        </Text>
+          <Ionicons
+            name="camera-outline"
+            size={24}
+            color={dark ? "#e5e7eb" : "#1f2937"}
+          />
+        </TouchableOpacity>
       </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginHorizontal: 10,
+          marginTop: 45,
+        }}
+      >
+        <View
+          style={{
+            marginLeft: 10,
+          }}
+        >
+          <Text
+            style={{
+              color: colors.text,
+
+              fontFamily: fonts.primary.regular,
+              fontSize: 18,
+            }}
+          >
+            {userInfo?.name}
+          </Text>
+          <Text
+            style={{
+              color: colors.text,
+              fontFamily: fonts.primary.regular,
+              fontSize: 14,
+              opacity: 0.4,
+            }}
+          >
+            {userInfo?.email}
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            backgroundColor: colors.primary,
+            padding: 10,
+            borderRadius: 10,
+          }}
+        >
+          <Text
+            style={{
+              color: colors.text,
+              fontFamily: fonts.primary.regular,
+              fontSize: 14,
+            }}
+          >
+            Edit Profile
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          flexDirection: "row",
+          marginHorizontal: 22,
+          marginVertical: 10,
+        }}
+      >
+        <Button
+          onPress={() => Linking.openURL(userInfo.website as string)}
+          variant="text"
+          style={{
+            borderBottomWidth: 2,
+            height: 20,
+            borderRadius: 0,
+            borderBottomColor: "#f2f2f2",
+          }}
+        >
+          Visit Website
+        </Button>
+      </View>
+      <SectionList
+        style={{
+          marginTop: 10,
+          paddingHorizontal: 20,
+        }}
+        sections={[
+          {
+            title: "Education",
+            data: [userInfo?.education],
+          },
+          {
+            title: "Address",
+            data: [userInfo?.address],
+          },
+        ]}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              borderRadius: 10,
+              marginBottom: 10,
+            }}
+          >
+            <View>
+              {Array.isArray(item) &&
+                item?.map((item) => {
+                  return (
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginHorizontal: 10,
+                        marginTop: 10,
+                      }}
+                    >
+                      <View>
+                        <Text>{item?.school}</Text>
+                        <Text>{item?.degree}</Text>
+                        <Text>{item?.fieldOfStudy}</Text>
+                      </View>
+                      <Text>{item?.from}</Text>
+                      <Text>{item?.to}</Text>
+                    </View>
+                  );
+                })}
+
+              {!Array.isArray(item) && (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginHorizontal: 10,
+                    marginTop: 10,
+                  }}
+                >
+                  <Text>
+                    {item?.street}, {item?.city}, {item?.state}, {item?.country}
+                  </Text>
+                </View>
+              )}
+            </View>
+          </View>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text
+            style={{
+              color: colors.text,
+              fontFamily: fonts.primary.regular,
+              fontSize: 18,
+              marginTop: 10,
+            }}
+          >
+            {title}
+          </Text>
+        )}
+      />
     </View>
   );
 }
